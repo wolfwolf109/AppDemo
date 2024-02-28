8b511bf82558fb82f09cd81debb7a4f72a73061c
wolfwolf109 <96578538+wolfwolf109@users.noreply.github.com>
2024-02-28 15:35:15 +0000

    frontend initialized

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,6 @@ import { Routes, Route, Outlet, Link } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <h1>Basic Example</h1>
-
-      <p>
-        This example demonstrates some of the core features of React Router
-        including nested <code>&lt;Route&gt;</code>s,{" "}
-        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-        "*" route (aka "splat route") to render a "not found" page when someone
-        visits an unrecognized URL.
-      </p>
 
       {/* Routes nest inside one another. Nested route paths build upon
             parent route paths, and nested route elements render inside
@@ -34,24 +25,29 @@ export default function App() {
 
 function Layout() {
   return (
-    <div>
+    <div >
       {/* A "layout route" is a good place to put markup you want to
           share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
+      <nav className="sticky top-0 bg-sky-950 flex flex-row h-20 p-3 text-white" >
+        <div className="p-4 bg-slate-500 w-1/2">
+          <Link to="/">Company Logo</Link>
+        </div>
+
+        <div className="p-4 hover:border-b-4">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="p-4 hover:border-b-4">
+          <Link to="/about">About Us</Link>
+        </div>
+        <div className="p-4 hover:border-b-4">
+          <Link to="/dashboard">Events and News</Link>
+        </div>
+        <div className="p-4 hover:border-b-4">
+          <Link to="/nothing-here">Contact Us</Link>
+        </div>
+        <div className="p-4 hover:border-b-4">
+          <Link to="/nothing-here">Login</Link>
+        </div>
       </nav>
 
       <hr />
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
+const root = createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
