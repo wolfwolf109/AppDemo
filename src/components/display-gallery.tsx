9aff8c99a9ccf63f38c70825a26d5d29d7f5acb9
wolfwolf109 <96578538+wolfwolf109@users.noreply.github.com>
2024-04-01 15:04:10 +0000

    gallery time changes

--- src/components/display-gallery.tsx
+++ src/components/display-gallery.tsx
@@ -29,7 +29,7 @@ export default function DisplayGallery() {
                     freeMode={true}
                     loop={true}
                     autoplay={{
-                        delay: 10000,
+                        delay: 2500,
                         disableOnInteraction: false,
                       }}
                     modules={[FreeMode, Autoplay]}
